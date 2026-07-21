30a4aa016c9cec443ebd86e3e98d6526e15943c0
Claire Iidea <danbua999@gmail.com>
2026-07-21 16:09:58 +0700

    again

--- LoadEnv.ts
+++ LoadEnv.ts
@@ -1,7 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
 export default {
     GITHUB_USERNAME: process.env.GITHUB_USERNAME ?? "",
     APPLICATION_ID: process.env.APPLICATION_ID ?? "",
--- api/update-widget.ts
+++ api/update-widget.ts
@@ -1,6 +1,8 @@
+import UpdateWidget from "../helpers/UpdateWidget.js";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 const Handler = async (_Req: VercelRequest, Res: VercelResponse): Promise<void>  => {
+    await UpdateWidget();
     Res.status(200).send("OK");
 };
 
