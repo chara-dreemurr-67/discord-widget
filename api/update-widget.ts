6e2dd97cad438f2b071985ab3dc5758e7602b3c4
Claire Iidea <danbua999@gmail.com>
2026-07-21 15:44:14 +0700

    again

--- api/update-widget.ts
+++ api/update-widget.ts
@@ -1,7 +1,7 @@
 import UpdateWidget from "../helpers/UpdateWidget.js";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-const Handler = async (Req: VercelRequest, Res: VercelResponse): Promise<void>  => {
+const Handler = async (_Req: VercelRequest, Res: VercelResponse): Promise<void>  => {
     await UpdateWidget();
     Res.status(200).send("OK");
 };
